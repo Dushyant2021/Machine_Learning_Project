2cc1795bd89298b01e561bd29b928b594b19bd0b
Dushyant2021 <dushyant2031@gmail.com>
2022-09-26 20:18:11 +0530

    update1

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app=Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Start Machine Learning Project"
+    return "CI CD pipelline has been established"
 
 
 if __name__=="__main__":
